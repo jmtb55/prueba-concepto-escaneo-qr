3a349f675a8e31eb0a856ba52dfd616a0f4a874a
Jesus M. Tenias B <jesus.miguel.tenias.belmonte@everis.com>
2019-08-30 10:19:31 -0400

    prueba con primera libreria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation,
+  ViewChild
+} from '@angular/core';
+import { QrScannerComponent } from 'angular2-qrscanner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'prueba-concepto-escaneo-qr';
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild(QrScannerComponent) qrScannerComponent: QrScannerComponent;
+  results: string = '';
+  ngOnInit() {
+    this.qrScannerComponent.getMediaDevices().then(devices => {
+      const videoDevices: MediaDeviceInfo[] = [];
+      for (const device of devices) {
+        if (device.kind.toString() === 'videoinput') {
+          videoDevices.push(device);
+        }
+      }
+      if (videoDevices.length > 0) {
+        let choosenDev;
+        for (const dev of videoDevices) {
+          if (dev.label.includes('front')) {
+            choosenDev = dev;
+            break;
+          }
+        }
+        if (choosenDev) {
+          this.qrScannerComponent.chooseCamera.next(choosenDev);
+        } else {
+          this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+        }
+      }
+    });
+
+    this.qrScannerComponent.capturedQr.subscribe(result => {
+      this.results = JSON.stringify({ result }, null, 2);
+    });
+  }
+
+  ngOnDestroy() {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgQrScannerModule } from 'angular2-qrscanner';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgQrScannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
